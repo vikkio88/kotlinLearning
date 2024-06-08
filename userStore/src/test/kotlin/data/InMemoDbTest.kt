ee5cb444f0a602e7affd9cb4fbd2c3fceee216ad
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2024-06-08 21:05:11 +0100

    fixed tests

--- userStore/src/test/kotlin/data/InMemoDbTest.kt
+++ userStore/src/test/kotlin/data/InMemoDbTest.kt
@@ -3,35 +3,36 @@ package data
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.vikkio.data.InMemoDb
-import org.vikkio.models.User
+import org.vikkio.models.UserFactory
 
 class InMemoDbTest {
 
     @Test
     fun addGetUsers() {
         val db = InMemoDb()
-        db.addUser(User("mario"))
+        db.addUser(UserFactory.makeUser("mario"))
         assertEquals(1, db.getUsers().count())
-        db.addUser(User("marione"))
+        db.addUser(UserFactory.makeUser("marione"))
         assertEquals(2, db.getUsers().count())
     }
 
     @Test
     fun passwordJourney() {
         val db = InMemoDb()
-        val user = User("Mariano Marione")
+        val user = UserFactory.makeUser("Mario")
         db.addUser(user)
-
-        assertNull(db.tryLogin(user.id, "PASSWORD"))
-        db.resetUserPassword(user.id, "PASSWORD")
-        assertNotNull(db.tryLogin(user.id, "PASSWORD"))
-        assertFalse(db.setUserPassword(user.id, "PASSWORD1", "WRONG"))
-        assertNull(db.tryLogin(user.id, "WRONG"))
-        assertTrue(db.setUserPassword(user.id, "PASSWORD1", "PASSWORD"))
-        assertNull(db.tryLogin(user.id, "PASSWORD"))
-        assertNotNull(db.tryLogin(user.id, "PASSWORD1"))
-        db.resetUserPassword(user.id, "MARIOPASSWORD")
-        assertNull(db.tryLogin(user.id, "PASSWORD1"))
-        assertNotNull(db.tryLogin(user.id, "MARIOPASSWORD"))
+        val username = user.username
+        val userId = user.id
+        assertNull(db.tryLogin(username, "PASSWORD"))
+        db.resetUserPassword(userId = userId, "PASSWORD")
+        assertNotNull(db.tryLogin(username, "PASSWORD"))
+        assertFalse(db.setUserPassword(userId = userId, "PASSWORD1", "WRONG"))
+        assertNull(db.tryLogin(username, "WRONG"))
+        assertTrue(db.setUserPassword(userId = userId, "PASSWORD1", "PASSWORD"))
+        assertNull(db.tryLogin(username, "PASSWORD"))
+        assertNotNull(db.tryLogin(username, "PASSWORD1"))
+        db.resetUserPassword(userId = userId, "MARIOPASSWORD")
+        assertNull(db.tryLogin(username, "PASSWORD1"))
+        assertNotNull(db.tryLogin(username, "MARIOPASSWORD"))
     }
 }
