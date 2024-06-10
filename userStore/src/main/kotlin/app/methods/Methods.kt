026f3657669ae69963ef746d7334c46ee7289c70
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2024-06-10 19:53:00 +0100

    adding delete and create new accout methods

--- userStore/src/main/kotlin/app/methods/Methods.kt
+++ userStore/src/main/kotlin/app/methods/Methods.kt
@@ -2,7 +2,7 @@ package org.vikkio.app.methods
 
 import org.vikkio.app.Context
 
-enum class Methods(val label: String, val body: ((Context)-> Unit)? = null) {
+enum class Methods(val label: String, val body: ((Context) -> Unit)? = null) {
     LOGIN("Login", login),
     LOGOUT("Logout", logout),
 
@@ -10,15 +10,18 @@ enum class Methods(val label: String, val body: ((Context)-> Unit)? = null) {
     ADD_USER("Add a user", addUser),
     LIST_USERS("List all users", listUsers),
     ADMIN_CHANGE_PASSWORD("Reset User Password", adminChangePassword),
+    DELETE_USER("Delete User", deleteUser),
 
     // Users
     // -> Money movements
     WITHDRAW("Withdraw", withdraw),
     DEPOSIT("Deposit", deposit),
     MOVE("Move"),
+
     // -> Account Utilities
     SELECT_ACCOUNT("Select Account", selectAccount),
-    RENAME_ACCOUNT("Select Account", renameAccount),
+    CREATE_NEWACCOUNT("Create New Account", createNewAccount),
+    RENAME_ACCOUNT("Rename Account", renameAccount),
 
 
     QUIT("Quit app"),
--- userStore/src/main/kotlin/app/methods/methodsConfig.kt
+++ userStore/src/main/kotlin/app/methods/methodsConfig.kt
@@ -5,14 +5,18 @@ import org.vikkio.app.AppState
 val adminMenuMethodMap = mapOf(
     arrayOf("a", "add") to Methods.ADD_USER,
     arrayOf("l", "ls", "list") to Methods.LIST_USERS,
-    arrayOf("acp") to Methods.ADMIN_CHANGE_PASSWORD,
+    arrayOf("cp") to Methods.ADMIN_CHANGE_PASSWORD,
+    arrayOf("du") to Methods.DELETE_USER,
     arrayOf("lo", "logout") to Methods.LOGOUT,
     arrayOf("q", "quit", "exit") to Methods.QUIT,
 )
 
 val userMenuMethodMap = mapOf(
-    arrayOf("w","withdraw") to Methods.WITHDRAW,
-    arrayOf("d","deposit") to Methods.DEPOSIT,
+    arrayOf("w", "withdraw") to Methods.WITHDRAW,
+    arrayOf("d", "deposit") to Methods.DEPOSIT,
+    arrayOf("ra", "rename:account") to Methods.RENAME_ACCOUNT,
+    arrayOf("sa", "select:account") to Methods.SELECT_ACCOUNT,
+    arrayOf("cna", "create:account") to Methods.CREATE_NEWACCOUNT,
     arrayOf("lo", "logout") to Methods.LOGOUT,
     arrayOf("q", "quit", "exit") to Methods.QUIT,
 )
