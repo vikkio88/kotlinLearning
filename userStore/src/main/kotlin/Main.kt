a50a26c7735de702d37b63ac24570b407d74757b
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2024-06-16 13:40:47 +0100

    testing sqlite

--- userStore/src/main/kotlin/Main.kt
+++ userStore/src/main/kotlin/Main.kt
@@ -1,12 +1,35 @@
 package org.vikkio
 
-import io.github.cdimascio.dotenv.dotenv
-import org.vikkio.app.App
+import java.sql.DriverManager
+import java.sql.ResultSet
 
 
 fun main() {
-    val env = dotenv()
-    val appSecret = env["APP_SECRET"] ?: "secret"
-    val app = App(appSecret)
-    app.run()
+//    val env = dotenv()
+//    val appSecret = env["APP_SECRET"] ?: "secret"
+//    val app = App(appSecret)
+//    app.run()
+    val connection = DriverManager.getConnection("jdbc:sqlite:test.db")
+
+
+    val stm = connection.prepareStatement(
+        """
+            CREATE TABLE IF NOT EXISTS users (  
+                id VARCHAR(255) PRIMARY KEY,
+                fullname VARCHAR(255) NOT NULL,
+                role TEXT NOT NULL CHECK(role IN ('USER', 'ADMIN')),
+                username VARCHAR(255) UNIQUE NOT NULL
+            );
+        """.trimIndent()
+    )
+    stm.execute()
+    stm.close()
+
+    val statement = connection.createStatement()
+    val rs: ResultSet = statement.executeQuery("select * from users")
+    while (rs.next()) {
+        println("name = " + rs.getString("name"))
+        println("id = " + rs.getInt("id"))
+    }
+
 }
