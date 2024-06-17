2932717484fb6de93fe6581d76892bf77ae7e5b4
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2024-06-17 20:29:26 +0100

    removing normdb

--- userStore/src/main/kotlin/Main.kt
+++ userStore/src/main/kotlin/Main.kt
@@ -2,8 +2,7 @@ package org.vikkio
 
 import io.github.cdimascio.dotenv.dotenv
 import org.vikkio.app.App
-import org.vikkio.data.jdbcsqlite.JdbcSqliteDb
-import org.vikkio.data.normDb.NormDb
+
 import org.vikkio.models.UserFactory
 
 
@@ -12,16 +11,17 @@ fun main() {
 //    val appSecret = env["APP_SECRET"] ?: "secret"
 //    val app = App(appSecret)
 //    app.run()
-    val db = NormDb()
-    db.boot()
 
 
-//    val l = UserFactory.makeUser("Facci Diminchiax")
+
+//    val db = NormDb()
+//    db.boot()
+
+
+//    val l = UserFactory.makeUser("Facci Di Minchia")
 //    db.addUser(l)
 
-    for (u in db.getUsers()) {
-        println(u)
-    }
+
 
 
 }
